10f020c358072d0027382dbb70f6e4e9d6a3ebd2
HAN <hanhan5556888@gmail.com>
2022-10-26 12:26:11 +0800

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float a = 5f;
+
+        double b = 5f;
+
+        float dist = Vector3.Distance(transform.position, target.position);
+        if(dist <6)
+        {
+            transform.localScale = new Vector3(2, 2, 3);
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }
+
+    }
+}
